7e7038cdb75249c0b58ee26dde354a1e684c39f0
Fabiano Júnior <fabianogmjunior@gmail.com>
2021-08-29 18:39:38 -0300

    init repo

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+class MainTest {
+
+    @Test
+    void exampleTest() {
+        assertThat(true).isTrue();
+    }
+}
