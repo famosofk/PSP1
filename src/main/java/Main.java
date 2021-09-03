6d746a17aa46a04d6518c43eae01f3dec82b7330
Fabiano Júnior <fabianogmjunior@gmail.com>
2021-09-03 09:23:38 -0300

    Add Main implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,67 @@
+import model.Defender;
+import model.Player;
+import model.Striker;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.err.println("Init project");
+    static FileInputStream is;
+    static DataInputStream data;
+    static BufferedReader reader;
+
+    public static void main(String[] args) throws IOException {
+        try {
+            initStreams(args);
+            handleInputFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closeStreams();
+        }
+    }
+
+    private static void handleInputFile() throws IOException {
+        while (true) {
+            String arguments = reader.readLine();
+            if (arguments.equals("0 0")) {
+                break;
+            } else {
+                checkOffside();
+            }
+        }
+    }
+
+    private static void checkOffside() throws IOException {
+        List<Striker> strikers = Striker.create(reader.readLine());
+        List<Defender> defenders = Defender.create(reader.readLine());
+        Player.verifyIfTheresOffsidePlayer(strikers, defenders);
+    }
+
+    private static void initStreams(String[] args) throws FileNotFoundException {
+        is = new FileInputStream("Teste.txt");
+        data = new DataInputStream(is);
+        reader = new BufferedReader(new InputStreamReader(data));
+    }
+
+    private static void closeStreams() throws IOException {
+        reader.close();
+        data.close();
+        is.close();
+    }
+
+    private void mockList() {
+        List<Striker> strikerList = new ArrayList<>();
+        List<Defender> defenderList = new ArrayList<>();
+
+        strikerList.add(new Striker(150));
+        strikerList.add(new Striker(90));
+        strikerList.add(new Striker(200));
+        defenderList.add(new Defender(150));
+        defenderList.add(new Defender(350));
+        defenderList.add(new Defender(250));
+        Player.verifyIfTheresOffsidePlayer(strikerList, defenderList);
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,4 +8,16 @@ class MainTest {
     void exampleTest() {
         assertThat(true).isTrue();
     }
+
+    @Test
+    void testReadPlayersInvolved() {
+        String readLine = "2 3";
+
+        String[] playersInvolved = readLine.split(" ");
+        Integer strikers = Integer.parseInt(playersInvolved[0]);
+        Integer defenders = Integer.parseInt(playersInvolved[1]);
+
+        assertThat(strikers).isEqualTo(2);
+        assertThat(defenders).isEqualTo(3);
+    }
 }
